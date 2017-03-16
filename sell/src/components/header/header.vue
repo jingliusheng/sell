d6bd88ce671a340bdd8f028d4073ae1280156a5f
jingliusheng <1454065462@qq.con>
2017-03-16 14:46:34 +0800

    test

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -17,8 +17,18 @@
           <span class="text">{{ seller.supports[0].description }}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{ seller.supports.length }}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{ seller.bulletin }}</span>
+      <span class="icon-keyboard_arrow_right"></span>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -40,10 +50,12 @@
 
   .header
     color: #fff
-    background: #000000
+    position: relative
+    background: rgba(7, 17, 27, 0.5)
     .content-wrapper
       padding: 24px 12px 18px 24px
       font-size: 0
+      position: relative
       .avatar
         display: inline-block
         vertical-align: top
@@ -95,4 +107,55 @@
           .text
               font-size: 12px
               line-height: 10px
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, 0.2)
+        .count
+          vertical-align: top
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          line-height: 24px
+          margin-left: 2px
+          font-size: 10px
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space:nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background-color: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 9px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        margin: 0 4px
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+    .background
+      position: absolute
+      top: 0
+      left: 0
+      width:100%
+      height:100%
+      z-index:-1
+      filter:blur(10px)
+
 </style>
